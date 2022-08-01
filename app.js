71e159fbe94d8551a62c0f36f5e3c4b4148a4cd4
Johannes Törmänen <johannes.tormanen@hotmail.fi>
2022-08-01 11:01:08 +0300

    added server side pagination to data querys

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const async = require('async')
 
 const csvMerger = require('csv-merger');
 
-var regexp_quote = require("regexp-quote");
+
 
 var mongoUrl = "mongodb://localhost:27017/SampleDb";
 
@@ -50,69 +50,85 @@ app.get('/bikeRoutes/load', async (req, res, next) => {
             
           });
         
-         
+        } catch {
+            res.status(500).send()
+        }
+
         options = {
             outputPath: "testi.csv", // string: path to the output CSV file
             writeOutput: true, // boolean: if true, the output will be written to a file, otherwise will be returned by the function 
         }
-        await csvMerger.merge(["2021-05.csv","2021-06.csv","2021-07.csv"], options);
-        var fileName =  options.outputPath;
+        try {
+            await csvMerger.merge(["2021-05.csv","2021-06.csv","2021-07.csv"], options);
+            var fileName =  options.outputPath;
+        } catch {
+            res.status(500).send()
+        }
 
-        var arrayToInsert = [];
         
-        await csvtojson().fromFile(fileName).then(source => {
-            // Fetching the all data from each row
-            for (var i = 0; i < source.length; i++) {
-                 var oneRow = {
-                     departure: source[i]["Departure"],
-                     return: source[i]["Return"],
-                     depStatID: source[i]["Departure station id"],
-                     retStatName: source[i]["Return station name"],
-                     retStatID: source[i]["Return station id"],
-                     depStatName: source[i]["Departure station name"],
-                     distance: source[i]["Covered distance (m)"],
-                     duration: source[i]["Duration (sec)"]
-                 };
-                 //Only add data with distance over 10m and duration over 10 sec
-                 if(oneRow.distance > 10 && oneRow.duration > 10) arrayToInsert.push(oneRow);
-             }
-
-             //inserting into the table "bikeRoutes"
-             var collectionName = 'bikeRoutes';
-            var collection = dbConn.collection(collectionName);
         
-             
-              collection.insertMany(arrayToInsert, (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return res.sendStatus(500);
-                    
+        try {
+            var arrayToInsert = [];
+            csvtojson().fromFile(fileName).then(source => {
+                // Fetching the all data from each row
+                for (var i = 0; i < source.length; i++) {
+                    var oneRow = {
+                        departure: source[i]["Departure"],
+                        return: source[i]["Return"],
+                        depStatID: source[i]["Departure station id"],
+                        retStatName: source[i]["Return station name"],
+                        retStatID: source[i]["Return station id"],
+                        depStatName: source[i]["Departure station name"],
+                        distance: source[i]["Covered distance (m)"],
+                        duration: source[i]["Duration (sec)"]
+                    };
+                    //Only add data with distance over 10m and duration over 10 sec 
+                    if(oneRow.distance > 10 && oneRow.duration > 10) arrayToInsert.push(oneRow);
                 }
-                if(result){
-                     console.log("Import CSV into database successfully.");
-                    
-                     
-                     
-                 }
-             });   
-            });
+                
+                //inserting into the table "bikeRoutes"
+                var collectionName = 'bikeRoutes';
+                var collection = dbConn.collection(collectionName);
+            
+                
+                collection.insertMany(arrayToInsert, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        //return res.sendStatus(500);
+                        
+                    }
+                    if(result){
+                        console.log("Import CSV into database successfully.");
+                        
+                        
+                        
+                    }
+                });   
+                });
+            } catch {
+                res.status(500).send()
+            }
                 
         
-    } catch {
-        res.status(500).send()
-    }
+    
     
     
 
     })
 
-    app.get('/bikeRoutes/:month-:limit', async (req, res, next) => {
+    app.get('/bikeRoutes/:month-:size-:pageNumber', async (req, res) => {
         try {
             var month ="2021-0"+ req.params.month; 
-            var limitt = parseInt(req.params.limit);
-            console.log(limitt);
+            var size = parseInt(req.params.size);
+            var pageNumber = parseInt(req.params.pageNumber);
+            
+            if(pageNumber < 0 || pageNumber === 0) {
+                response = {"error" : true,"message" : "invalid page number, should start with 1"};
+                return res.json(response)
+          }
+
             var query =  { "return": {$in:[new RegExp(month)]} };
-            await dbConn.collection("bikeRoutes").find(query).limit(limitt).toArray(function(err, result) {
+            await dbConn.collection("bikeRoutes").find(query).skip(size * (pageNumber - 1)).limit(size).toArray(function(err, result) {
                 if (err) {
                     console.log(err);
                     
@@ -123,7 +139,7 @@ app.get('/bikeRoutes/load', async (req, res, next) => {
                     res.status(500).send()
                 }
                 });
-                    
+
             
         } catch {
             res.status(500).send()
