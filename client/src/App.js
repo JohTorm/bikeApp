865f46bf153dff8fb12101e19c6126c2877c1c5e
Johannes Törmänen <johannes.tormanen@hotmail.fi>
2022-08-02 12:31:31 +0300

    Added table with pagination to show data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import DataTable from 'react-data-table-component';
+
+
+import "./App.css";
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+	const [totalRows, setTotalRows] = useState(100);
+	const [perPage, setPerPage] = useState(10);
+  const [month, setMonth] = useState(5);
+  const [page1, setPage1] = useState(1);
+  
+  
+
+  
+
+  const fetchUsers = async (month,perPage,page) => {
+		setLoading(true);
+    setPage1(page);
+    setMonth(month);
+		await axios.get("/bikeRoutes/" +month+ "-" +perPage+"-"+page1).then((res) => {
+      setData(res.data);
+      
+    })
+    .catch((err) => console.log(err));;
+
+		
+		setLoading(false);
+	};
+
+	const handlePageChange = page => {
+		setLoading(true);
+    fetchUsers(month,perPage,page);
+    setLoading(false);
+	};
+
+	const handlePerRowsChange = async (newPerPage, page) => {
+		setLoading(true);
+
+		setPage1(page);
+    setPerPage(newPerPage);
+		fetchUsers(month,perPage,page);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchUsers(5,10,1);
+		
+	}, []);
+  const columns = [
+    {
+      name: "Departure",
+      selector: row => row.departure,
+		  sortable: true
+    },
+    {
+      name: "return",
+      selector: row => row.return,
+		  sortable: true
+    },
+    {
+      name: "Departure station name",
+      selector: row => row.depStatName,
+		  sortable: true
+    },
+    {
+      name: "return station name",
+      selector: row => row.retStatName,
+		  sortable: true
+    },
+    {
+      name: "distance",
+      selector: row => row.distance,
+		  sortable: true
+    },
+    {
+      name: "duration",
+      selector: row => row.duration,
+		  sortable: true
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <center>React Table Demo</center>
+      </h1>
+      
+      <DataTable
+			title="Biking trips"
+			columns={columns}
+			data={data}
+			progressPending={loading}
+			pagination
+			paginationServer
+			paginationTotalRows={totalRows}
+			selectableRows
+			onChangeRowsPerPage={handlePerRowsChange}
+			onChangePage={handlePageChange}
+		/>
     </div>
   );
 }
